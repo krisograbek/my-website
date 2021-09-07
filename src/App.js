623d3048ed100c2f0fc9a5e7483964d6ffa221b1
krisograbek <krzysztof.ograbek@gmail.com>
2021-09-07 12:28:28 +0200

    added links for authors, some cleanups

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import './App.css';
-import { makeStyles } from '@material-ui/core';
-import { Grid, Paper } from '@material-ui/core';
-import Test from './components/Test';
-import Navbar from './components/Navbar';
 import PageLayout from './components/PageLayout';
 
 
--- src/components/FavBooks.js
+++ src/components/FavBooks.js
@@ -1,8 +1,44 @@
 import React from "react";
+import { Grid, Link } from "@material-ui/core";
+import books from './data/books.json'
+
 
 function FavBooks() {
+  const bookKeys = Object.keys(books);
+
   return (
-    <h1>FavBooks</h1>
+    <Grid item>
+      <h1>Favorite Books</h1>
+      {bookKeys.map((bookKey, id) =>
+        <div key={id}>
+          <h1>{bookKey}</h1>
+          {books[bookKey].map((book, key) =>
+            <Grid item key={key}>
+              <Link
+                href={book.link}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {book.title}
+              </Link> by
+              {
+                book.authorLink ?
+                  <Link href={book.authorLink}> {book.author} </Link> :
+                  <span> {book.author} </span>
+              }
+              {/* <p>What's this book about? {book.description}</p> */}
+              <p>Why this book?</p>
+              <div>
+                {
+                  book.impact
+                }
+              </div>
+              <hr />
+            </Grid>
+          )}
+        </div>
+      )}
+    </Grid >
   )
 }
 
