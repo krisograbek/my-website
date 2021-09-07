77b5212ad43e470cba42049a656693ac23db22af
krisograbek <krzysztof.ograbek@gmail.com>
2021-09-07 11:22:16 +0200

    create .json for articles, map through them using Link

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,23 +1,25 @@
 import React from "react";
 import { Link } from "@material-ui/core";
 import { Grid } from "@material-ui/core";
+import articles from "./data/articles.json"
 
 
 function Blog() {
   return (
     <Grid item>
       <h1>Blog</h1>
-      <Link
-        href="https://kris-ograbek-nlp.medium.com/simple-yet-powerful-an-introduction-to-named-entities-ce4fad33f217"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Simple Yet Powerful: An Introduction to Named Entities
-      </Link>
-      <Link href="https://kris-ograbek-nlp.medium.com/pay-attention-to-your-neighbors-a-simple-approach-to-the-part-of-speech-tagging-f8aa4af0b313">
-        Pay Attention to your Neighbors
-      </Link>
-    </Grid>
+      {articles.map((article, key) =>
+        <Grid item key={key}>
+          <Link
+            href={article.link}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {article.title}
+          </Link>
+        </Grid>
+      )}
+    </Grid >
   )
 }
 
