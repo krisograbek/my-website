34fc4a3c2dc5b3c9fc64a90937695e91204c4ba7
krisograbek <krzysztof.ograbek@gmail.com>
2021-09-18 08:58:25 +0200

    begin writing in about section, add emojis

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,40 @@
+import { Grid, makeStyles } from "@material-ui/core";
 import React from "react";
+import about from "./data/about.json"
+
+const useStyles = makeStyles(() => ({
+  root: {
+    padding: '30px'
+  }
+}));
 
 function About() {
+  const classes = useStyles();
+
   return (
-    <h1>About</h1>
+    <Grid
+      item
+      className={classes.root}
+    >
+      <Grid
+        container
+        direction="column"
+        spacing={2}
+      >
+        {
+          about.map((el, i) => {
+            return (
+              <Grid item>
+                <div key={i}>
+                  {el.emoji ? <span style={{ fontSize: "20px" }}>{el.emoji} </span> : <div />}
+                  {el.text}
+                </div>
+              </Grid>
+            )
+          })
+        }
+      </Grid>
+    </Grid>
   )
 }
 
